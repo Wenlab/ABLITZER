9aa039b1dba102517ec45e93e29d0382a4a83b3c
Danyang Li <ldy123@mail.ustc.edu.cn>
2018-08-14 20:31:47 +0800

    create a new file to annotate the video.

---
+++ Utility functions/annotateRawVideo_cut.m
@@ -0,0 +1,48 @@
+% TODO: add a GUI for reading video
+% TODO: add press esc to quit
+function annotateRawVideo_cut(expData)
+% Add a commment
+    startFrame = 21800;
+   [f,p] = uigetfile('*.avi');
+    videoName = [p,f];
+     newFileName = strcat(p,'new_',f);
+    newVObj = VideoWriter(newFileName);
+    open(newVObj);
+    
+yDiv =209;
+vObj = VideoReader(videoName);
+h = figure;
+for i=21800:24000
+    if ~hasFrame(vObj)
+        return;
+    end
+    img = readFrame(vObj);
+    if (mod(i,100) == 0)
+        disp(i);
+    end
+    
+    if (i > startFrame)
+        figure(h.Number);
+        cla reset; % clear prior image
+        imshow(img,[]);
+        hold on;
+       
+        line([0,366],[yDiv,yDiv],...
+            'color','r','lineWidth',1.5);
+     
+        
+        if (expData.PatternIdx2(i) == 0)
+            text(20,10, 'CS TOP');
+        elseif (expData.PatternIdx2(i) == 1)
+            text(20, 10, 'CS BOTTOM');
+        
+      
+        end
+        
+        writeVideo(newVObj,img);
+    end
+        
+  
+        pause(0.1);
+end
+end
