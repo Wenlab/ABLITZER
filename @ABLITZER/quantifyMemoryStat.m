73d1f2715713b5e2c770683852809f528d94199a
Danyang Li <ldy123@mail.ustc.edu.cn>
2018-08-23 16:29:25 +0800

    test

--- @ABLITZER/quantifyMemoryStat.m
+++ @ABLITZER/quantifyMemoryStat.m
@@ -7,17 +7,17 @@ function quantifyMemoryStat(obj)
         scores = fish.Res.PItime(4).Scores;
         s = scores > 0; % convert it to proportion score
         sSum = cat(1,sSum,s(1:lenTest)');
-        
-        
-        
+
+
+
     end
     x = 1:lenTest;
     y = mean(sSum,1);
     errBar = std(sSum,1,1)/size(sSum,1);
-    
+
     shadedErrorBar(x,y,errBar);
     ylim([0,1]);
-    
-    
+
+
 
 end
