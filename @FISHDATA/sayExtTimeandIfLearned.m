c8a997c8503f7d16271e1f3319aaf9780a302dac
Danyang Li <ldy123@mail.ustc.edu.cn>
2018-09-26 17:30:12 +0800

    Modify the error that divide All into the Learners and non-Learners

--- @FISHDATA/sayExtTimeandIfLearned.m
+++ @FISHDATA/sayExtTimeandIfLearned.m
@@ -13,33 +13,16 @@ function [extTime,num_trail,Test_trail,meanBT] = sayExtTimeandIfLearned(obj)
   Baseline_trail=measure_trail(obj,1);
   Test_trail=measure_trail(obj,4);
   meanBT=mean(Baseline_trail);
-  idx=find(Test_trail>meanBT);
-  a1=isempty(find(idx==1));
-  a2=isempty(find(idx==2));
-  a3=isempty(find(idx==3));
-  a4=isempty(find(idx==4));
-  a5=isempty(find(idx==5));
-  if a1==1
-     num_trail=0 ;
-  elseif a2==1
-      num_trail=1;
-      elseif a3==1
-      num_trail=2;
-      elseif a4==1
-      num_trail=3;
-      elseif a5==1
-      num_trail=4;
-  else
-      num_trail=5;
-  end
-  if num_trail<3
+  idx=find(Test_trail<meanBT,1);
+  num_trail=idx-1;
+  if num_trail<2
        extTime=[];
        h = 0;
        p = nan;
-     else
+  else
      extTime = num_trail*120;  % seconds
-    [h,p] = ttest2(Baseline_trail,Test_trail);
+    [h,p] = ttest2(Baseline_trail,Test_trail(1:num_trail));
    end
     obj.Res.ExtinctTime = extTime;
-     obj.Res.IfLearned = h;
+    obj.Res.IfLearned = h;
 end
