2aa0b60559541ea162b9ca0f8917519e86019751
Danyang Li <ldy123@mail.ustc.edu.cn>
2018-09-26 17:36:51 +0800

    Modular function and separate calculation from drawing

--- Utility functions/plot_bar_Iflearned.m
+++ Utility functions/plot_bar_Iflearned.m
@@ -1,42 +1,49 @@
-function [p,output_Ls,output_nLs,output_All] = plot_bar_Iflearned(a)
-[Ls,nLs] = divideByIflearned(a)
+function [p,Ls,nLs,All] = plot_bar_Iflearned(Ls,nLs)
+%get [Ls,nLs] by function divideByIflearned(a);
 
-output_All = output_task(a,"OLexp");
-output_Ls = output_task(Ls,"OLexp");
-output_nLs = output_task(nLs,"OLexp");
-%aver = struct('IfLearned',[],'PITime_Baseline',[],'PITime_Test',[],'PITurn_Baseline',[],'PITurn_Test',[]);
-%aver(1).IfLearned="Learners";
-%aver(2).IfLearned="Non-learners";
-%aver(3).IfLearned="All";
+alls.FishStack = cat(2,Ls.FishStack,nLs.FishStack);
+All = output_task(alls,"OLexp");
+Ls = output_task(Ls,"OLexp");
+nLs = output_task(nLs,"OLexp");
 
+PIs.Ls=[Ls.PITime_Baseline;Ls.PITime_Test;Ls.PITurn_Baseline;Ls.PITurn_Test];
+PIs.nLs=[nLs.PITime_Baseline;nLs.PITime_Test;nLs.PITurn_Baseline;nLs.PITurn_Test];
+PIs.All=[All.PITime_Baseline;All.PITime_Test;All.PITurn_Baseline;All.PITurn_Test];
 
-y11=[mean(output_Ls.PITime_Baseline),mean(output_nLs.PITime_Baseline),mean(output_All.PITime_Baseline)];
-y12=[mean(output_Ls.PITime_Test),mean(output_nLs.PITime_Test),mean(output_All.PITime_Test)];
-
-y1_std=[std(output_Ls.PITime_Baseline,1)/sqrt(length(output_Ls.PITime_Baseline)),std(output_Ls.PITime_Test,1)/sqrt(length(output_Ls.PITime_Test)),...
-      std(output_nLs.PITime_Baseline,1)/sqrt(length(output_nLs.PITime_Baseline)),std(output_nLs.PITime_Test,1)/sqrt(length(output_nLs.PITime_Test)),...
-      std(output_All.PITime_Baseline,1)/sqrt(length(output_All.PITime_Baseline)),std(output_All.PITime_Test,1)/sqrt(length(output_All.PITime_Test))];
+[Mean,Sem] = calcMeanSem(PIs);
 
 XTickLabel = ["Learners","non-Learners","All"];
-plotpairbar(y11,y12,y1_std,XTickLabel,"Positional Index");
+plotpairbar(Mean(1,:),Mean(2,:),Sem(1:6),XTickLabel,"Positional Index");
 
-p.time(1) = significanceTest(output_Ls.PITime_Baseline,output_Ls.PITime_Test,1);
-p.time(2) = significanceTest(output_nLs.PITime_Baseline,output_nLs.PITime_Test,4);
-p.time(3) = significanceTest(output_All.PITime_Baseline,output_All.PITime_Test,7);
+
+p.time(1) = significanceTest(PIs.Ls(1,:),PIs.Ls(2,:),1);
+p.time(2) = significanceTest(PIs.nLs(1,:),PIs.nLs(2,:),4);
+p.time(3) = significanceTest(PIs.All(1,:),PIs.All(2,:),7);
+
 legend('Before training','After training');
 
-y21=[mean(output_Ls.PITurn_Baseline),mean(output_nLs.PITurn_Baseline),mean(output_All.PITurn_Baseline)];
-y22=[mean(output_Ls.PITurn_Test),mean(output_nLs.PITurn_Test),mean(output_All.PITurn_Test)];
+plotpairbar(Mean(3,:),Mean(4,:),Sem(7:12),XTickLabel,"Turning Index");
+p.turn(1) = significanceTest(PIs.Ls(3,:),PIs.Ls(4,:),1);
+p.turn(2) = significanceTest(PIs.nLs(3,:),PIs.nLs(4,:),4);
+p.turn(3) = significanceTest(PIs.All(3,:),PIs.All(4,:),7);
 
-y2_std=[std(output_Ls.PITurn_Baseline,1)/sqrt(length(output_Ls.PITurn_Baseline)),std(output_Ls.PITurn_Test,1)/sqrt(length(output_Ls.PITurn_Test)),...
-      std(output_nLs.PITurn_Baseline,1)/sqrt(length(output_nLs.PITurn_Baseline)),std(output_nLs.PITurn_Test,1)/sqrt(length(output_nLs.PITurn_Test)),...
-      std(output_All.PITurn_Baseline,1)/sqrt(length(output_All.PITurn_Baseline)),std(output_All.PITurn_Test,1)/sqrt(length(output_All.PITurn_Test))];
-
-plotpairbar(y21,y22,y2_std,XTickLabel,"Turning Index");
-
-p.turn(1) = significanceTest(output_Ls.PITurn_Baseline,output_Ls.PITurn_Test,1);
-p.turn(2) = significanceTest(output_nLs.PITurn_Baseline,output_nLs.PITurn_Test,4);
-p.turn(3) = significanceTest(output_All.PITurn_Baseline,output_All.PITurn_Test,7);
 legend('Before training','After training');
 
+end
+
+
+
+function [Mean2,Sem] = calcMeanSem(PIs)
+Mean = [mean(PIs.Ls,2);mean(PIs.nLs,2);mean(PIs.All,2)];
+Mean2 = [Mean(1),Mean(5),Mean(9);Mean(2),Mean(6),Mean(10);...        %%PITimeBaselineMean;PITimeTestMean;
+         Mean(3),Mean(7),Mean(11);Mean(4),Mean(8),Mean(12)];        %%PITurnBaselineMean;PITurnTestMean;
+Sem(1:4) = calcSem(PIs.Ls);
+Sem(5:8) = calcSem(PIs.nLs);
+Sem(9:12) = calcSem(PIs.All);
+end
+
+function [Sem]=calcSem(Task)
+for i=1:4
+    Sem(i) = std((Task),1,2)/sqrt(length(Task(i,:)));
+end
 end
