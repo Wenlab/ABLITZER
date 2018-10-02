be6fd74a9e287a562e2a39c09d15858433db89a3
WanzhuoShi <shiwanzhuo@mail.ustc.edu.cn>
2018-10-02 21:04:49 +0800

    add a script for testing calculating PI functions

---
+++ testPIcalc.m
@@ -0,0 +1,9 @@
+% this script is for testing the PI calculating functions
+% aObj = ABLITZER, and the name of object must be "aObj"
+for n = 1:4
+    fish = aObj.FishStack(n);
+    fish.evaluateDataQuality;
+    fish.calcPItime;
+    fish.calcPIshock;
+    fish.calcPIturn;
+end
