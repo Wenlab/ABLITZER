552dfad2faac835d4bd556e02d76c224c896f582
young24 <young24@mail.ustc.edu.cn>
2018-11-01 17:22:31 +0800

    clean up some legacies

--- testPIcalc.m
+++
@@ -1,6 +0,0 @@
-% this script is for testing the PI calculating functions
-% aObj = ABLITZER, and the name of object must be "aObj"
-for n = 1:24
-    fish = obj1.FishStack(n);
-    fish.sayIfLearned;
-end
