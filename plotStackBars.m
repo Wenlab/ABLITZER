0069c29730b1926b3ea2222f36becfcbc1036571
WanzhuoShi <shiwanzhuo@mail.ustc.edu.cn>
2018-12-05 20:26:23 +0800

    update plotting function

--- plotStackBars.m
+++ plotStackBars.m
@@ -1,32 +1,28 @@
-y = zeros(3,3);
-MlStack = Memory
-FishStack = MemoryLengthStack(3).SmartfishStack;
-ML = cat(1,FishStack.MemoryLength);
-meanML = round(mean(ML));
-for n = 1:length(FishStack)
-    if FishStack(n).MemoryLength >= 240 && FishStack(n).MemoryLength <= 360
-        y(1) = y(1) + 1;
-    elseif FishStack(n).MemoryLength >= 480 && FishStack(n).MemoryLength <= 720
-        y(2) = y(2) + 1;
-    elseif FishStack(n).MemoryLength >= 840 && FishStack(n).MemoryLength <= 960
-        y(3) = y(3) + 1;
-    elseif FishStack(n).MemoryLength == 1080
-        y(4) = y(4) + 1;
+y = zeros(4,4);
+for k = 1:4
+    FishStack = MemLenStat_GCaMP(k).SmartfishStack; 
+    for n = 1:length(FishStack)
+        if FishStack(n).MemoryLength > 0 && FishStack(n).MemoryLength < 400
+            y(k,1) = y(k,1) + 1;
+        elseif FishStack(n).MemoryLength >= 400 && FishStack(n).MemoryLength < 800
+            y(k,2) = y(k,2) + 1;
+        elseif FishStack(n).MemoryLength >= 800 && FishStack(n).MemoryLength <= 1000
+            y(k,3) = y(k,3) + 1;
+        elseif FishStack(n).MemoryLength == 1080;
+            y(k,4) = y(k,4) + 1;
+        end
     end
 end
-x = 300:300:1200;
-h = bar(x,y);
-h.BarWidth = 0.5;
-h.FaceColor = [0 0 0];
 
-line([meanML,meanML],ylim,'Color','m');
-ticks = get(gca,'XTick');
-ticklabels = {'240~360','480~720','840~960','Not ectinct'};
-[ticks,idx] = sort(ticks);
-ticklabels = ticklabels(idx);
-set(gca,'Xtick',ticks,'XTickLabel',ticklabels);
-StrMean = mat2str(meanML);
+x = categorical({'RGB0','RGB32','RGB43','RGB64'});
 
+h = bar(x,y,'stacked');
+for n = 1:4
+    h(n).BarWidth = 0.5;
+end
 
-xlabel('Memory Length(s)');
+xlabel('CS Pattern');
 ylabel('Count');
+legend('0~400','400~800','800~1000','Not ectinct');
+
+
